2cf652d01110bc574a22b1062109fcf7172cd014
Dauris Little <dlittle@wells.edu>
2021-06-25 15:41:04 -0400

    Create ViewController.kt
    Create Controller directory
    create ViewController  to handle view

---
+++ kotlin/AdvanceSnackbar/app/src/main/java/com/programmingninja/advancesnackbar/controller/ViewController.kt
@@ -0,0 +1,35 @@
+package com.programmingninja.advancesnackbar.controller
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+
+internal fun View?.findParent(): ViewGroup? {
+
+    var view = this
+    var fallback: ViewGroup? = null
+
+    do {
+        if (view is CoordinatorLayout) {
+            //the Coordinatorlayout has been found
+            return view
+        } else if (view is FrameLayout) {
+            if (view.id == android.R.id.content) {
+                //
+                return view
+            } else {
+                //if no view assign the fallback
+                fallback = view
+            }
+        }
+
+        if (view != null) {
+            //will continue to search for the view
+            val parent = view.parent
+            view = if (parent is View) parent else null
+        }
+    } while (view != null)
+
+    return fallback
+}
