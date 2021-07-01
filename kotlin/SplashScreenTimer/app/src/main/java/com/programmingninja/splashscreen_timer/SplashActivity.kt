a80c2ea6e952e8bb2d8d7f78296105b58e753c44
Dauris Little <dlittle@wells.edu>
2021-07-01 11:39:44 -0400

    Update SplashActivity.kt
    use the Handler (It allows communicating back with UI thread from other background thread)
    Note: handler is technically deprecated

--- kotlin/SplashScreenTimer/app/src/main/java/com/programmingninja/splashscreen_timer/SplashActivity.kt
+++ kotlin/SplashScreenTimer/app/src/main/java/com/programmingninja/splashscreen_timer/SplashActivity.kt
@@ -1,11 +1,23 @@
 package com.programmingninja.splashscreen_timer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
+
+    //setting the timer for the activity
+    private val SPLASH_TIMER:Long = 5000 // this equates to seconds
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        //fire after timer expires
+        Handler().postDelayed({
+            //start the main activity
+            startActivity(Intent(this,MainActivity::class.java))
+        }, SPLASH_TIMER)
     }
 }
