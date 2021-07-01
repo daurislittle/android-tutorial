a87f18aeb95a654191594673fffeb06bead8a0e4
Dauris Little <dlittle@wells.edu>
2021-07-01 15:30:19 -0400

    Create Activity and Layout
    Created:
     - SmartSplashActivity
    - activity_smart_splash
    Update:
    - activity_smart_splash
    --> modify the layout to resemble a splash screen

---
+++ kotlin/SplashScreenSmartTimer/app/src/main/java/com/programmingninja/splashscreen_smarttimer/SmartSplashActvity.kt
@@ -0,0 +1,13 @@
+package com.programmingninja.splashscreen_smarttimer
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.content.ContextCompat
+
+class SmartSplashActvity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_smart_splash_actvity)
+    }
+}
