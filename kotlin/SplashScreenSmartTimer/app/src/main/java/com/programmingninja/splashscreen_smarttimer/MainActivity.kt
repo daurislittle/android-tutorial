3eca1f7ee44681841a3e804a80adce73a8823805
Dauris Little <dlittle@wells.edu>
2021-07-01 10:26:38 -0400

    Creation Smart Timer
    Creating the tutorial for the smart timer

---
+++ kotlin/SplashScreenSmartTimer/app/src/main/java/com/programmingninja/splashscreen_smarttimer/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.programmingninja.splashscreen_smarttimer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
