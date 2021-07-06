7d1ebc1b5e7e7b53c0197680a66979a32b15ebe1
Dauris Little <dlittle@wells.edu>
2021-07-06 14:23:33 -0400

    View Binding - Java Completed
    When user click submit button a toast message displays

--- java/ViewBinding/app/src/main/java/com/programmingninja/viewbinding/MainActivity.java
+++ java/ViewBinding/app/src/main/java/com/programmingninja/viewbinding/MainActivity.java
@@ -3,12 +3,43 @@ package com.programmingninja.viewbinding;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.programmingninja.viewbinding.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    //binding class to the xml
+    //allows the system to automatically generate the system
+    ActivityMainBinding amb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        //inflating the xml
+        amb = ActivityMainBinding.inflate(getLayoutInflater());
+
+        //retrieve the root layout
+        View v = amb.getRoot();
+
+        //ContentView for the layout
+        setContentView(v);
+
+        //calling the button and setting the click listener
+        //call the button by id and set the click listener
+        amb.btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+                String msg = amb.etMessage.getText().toString();
+                if (!msg.isEmpty()) Toast.makeText(v.getContext(), amb.etMessage.getText().toString(), Toast.LENGTH_SHORT).show();
+                else Toast.makeText(v.getContext(), "Message is empty", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
