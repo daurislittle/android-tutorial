9c4d4947fe5833722d868addad1d8edc6254d405
Dauris Little <dlittle@wells.edu>
2021-07-06 14:50:32 -0400

    View Binding Kotlin
    Completed project with Kotlin

--- kotlin/ViewBinding/app/src/main/java/com/programmingninja/viewbinding/MainActivity.kt
+++ kotlin/ViewBinding/app/src/main/java/com/programmingninja/viewbinding/MainActivity.kt
@@ -2,10 +2,27 @@ package com.programmingninja.viewbinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.programmingninja.viewbinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    //create instance ActivityMainBinding
+    private lateinit var amb : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        //create the instance of ActivityMainBinding
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+
+        //binding.root returns the root layout
+        setContentView(binding.root)
+
+        binding.btnSubmit.setOnClickListener {
+            val msg = binding.etMessage.text.toString()
+            if (!msg.isEmpty()) Toast.makeText(this, binding.etMessage.text.toString(), Toast.LENGTH_SHORT).show()
+            else Toast.makeText(this, "Message is currently empty", Toast.LENGTH_SHORT).show()
+        }
     }
 }
