ebbbd2a3c51f0114f36c1aeeb38b250a1c5d5da2
Dauris Little <dlittle@wells.edu>
2021-07-07 14:46:46 -0400

    Completed
    Updated:
     - activity added to manifest
    Created:
     - created layout
    Deleted:
     - remove this layout

--- kotlin/SimpleRecyclerview/app/src/main/java/com/programmingninja/simplerecyclerview/adapter/FarmAdapter.kt
+++ kotlin/SimpleRecyclerview/app/src/main/java/com/programmingninja/simplerecyclerview/adapter/FarmAdapter.kt
@@ -1,26 +1,37 @@
 package com.programmingninja.simplerecyclerview.adapter
 
 import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.programmingninja.simplerecyclerview.R
+import kotlinx.android.synthetic.main.activity_farm_adapter.view.*
 
-//allow the system to create the needed functions
-class FarmAdapter (val items : MutableList<String>, val context: Context) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class FarmAdapter(val items : MutableList<String>, val context: Context) : RecyclerView.Adapter<ViewHolder>() {
 
     //inflate the items in
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(context).inflate(R.layout.activity_farm_adapter, parent,false))
+
     }
 
     //bind the animal from the list to a view
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.farm_Animal.text = items[position]
     }
 
 
     //get the number of animals in the list
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return items.size
     }
 
+
+}
+
+class ViewHolder (view: View) : RecyclerView.ViewHolder(view) {
+    //holds the appcompattextview that will add each animal
+    val farm_Animal = view.tv_item
 }
