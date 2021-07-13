d1bd11ac9b51b85c536b3835a70acb2697a43ae2
Dauris Little <dlittle@wells.edu>
2021-07-13 14:50:51 -0400

    Update DatabaseContract.kt
    Inner class that defines the table contents

--- kotlin/LocalDatabaseSqlite/app/src/main/java/com/programmingninja/localdatabasesqlite/contract/DatabaseContract.kt
+++ kotlin/LocalDatabaseSqlite/app/src/main/java/com/programmingninja/localdatabasesqlite/contract/DatabaseContract.kt
@@ -1,4 +1,18 @@
 package com.programmingninja.localdatabasesqlite.contract
 
-class DatabaseContract {
+import android.provider.BaseColumns
+
+object DatabaseContract {
+
+    //
+    class CommitUserContent : BaseColumns {
+        companion object {
+            val TABLE_NAME = "" //TABLE NAME
+            val COLUMN_FIRST_NAME = "firstname" //column field
+            val COLUMN_LAST_NAME = "lastname" //column field
+            val COLUMN_USER_ID = "userid" //column field
+            val COLUMN_PASSWORD = "password" //column field
+            val COLUMN_DATE_CREATED = "date-created" //column field
+        }
+    }
 }
