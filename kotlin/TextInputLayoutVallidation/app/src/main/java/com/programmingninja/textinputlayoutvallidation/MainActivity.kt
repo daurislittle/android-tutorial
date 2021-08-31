5536dd22d9fc7c8d0296cbbda29e4f1a51d5646b
Dauris Little <dlittle@wells.edu>
2021-08-31 16:00:20 -0400

    Update MainActivity
    Supply regex on valid controller

--- kotlin/TextInputLayoutVallidation/app/src/main/java/com/programmingninja/textinputlayoutvallidation/MainActivity.kt
+++ kotlin/TextInputLayoutVallidation/app/src/main/java/com/programmingninja/textinputlayoutvallidation/MainActivity.kt
@@ -7,7 +7,10 @@ import android.text.TextWatcher
 import android.view.View
 import androidx.databinding.DataBindingUtil
 import com.google.android.material.snackbar.Snackbar
+import com.programmingninja.textinputlayoutvallidation.ValidateMe.validEmailFormat
+import com.programmingninja.textinputlayoutvallidation.ValidateMe.validPasswordFormat
 import com.programmingninja.textinputlayoutvallidation.databinding.ActivityMainBinding
+import java.util.regex.Pattern
 
 class MainActivity : AppCompatActivity() {
 
@@ -39,17 +42,24 @@ class MainActivity : AppCompatActivity() {
     /**
      * validate fields
      */
-    private fun  fieldValidation() : Boolean = validateName() && validateEmail() && validatePassword()
+    private fun  fieldValidation() : Boolean = validateFirstName() && validateLastName() && validateEmail() && validatePassword()
 
     /**
      * fields validation that review that the following fields are not empty
      *  - validatePassword (will check that password is not empty)
+     *      - make sure user follow password requirement
      *  - validateEmail (will check that email is not empty)
+     *      - make sure user use an actual email
      *  - validateName (will check first & last name is not empty)
+     *      - make sure user not just putting anything here
      */
     private fun validatePassword(): Boolean {
         if (binding.txtInputEditPass.text.toString().trim().isEmpty()) {
             binding.txtLayoutPass.error = "Excuse me sir, this is a required field"
+            binding.txtInputEditPass.requestFocus()
+            return false
+        } else if (!validPasswordFormat(binding.txtInputEditPass.text.toString())) {
+            binding.txtLayoutPass.error = "Excuse me sir, password does not meet expectation"
             binding.txtInputEditPass.requestFocus()
             return false
         } else {
@@ -63,6 +73,9 @@ class MainActivity : AppCompatActivity() {
             binding.txtLayoutEmail.error = "Excuse me sir, this is a required field"
             binding.txtInputEditEmail.requestFocus()
             return false
+        } else if (!validEmailFormat(binding.txtInputEditEmail.text.toString())) {
+            binding.txtLayoutEmail.error = "Excuse me sir, password does not meet expectation"
+            binding.txtInputEditEmail.requestFocus()
         } else {
             binding.txtLayoutEmail.isErrorEnabled = false
 
@@ -70,18 +83,25 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
-    private fun validateName(): Boolean {
+    private fun validateFirstName(): Boolean {
         //validate first & last name
         if (binding.txtInputEditFname.text.toString().trim().isEmpty()) {
             binding.txtLayoutFname.error = "Excuse me sir, this is a required field"
             binding.txtInputEditFname.requestFocus()
             return false
-        } else if (binding.txtInputEditLname.text.toString().trim().isEmpty()) {
+        } else {
+            binding.txtLayoutFname.isErrorEnabled = false
+        }
+        return true
+    }
+
+    private fun validateLastName(): Boolean {
+        //validate first & last name
+        if (binding.txtInputEditLname.text.toString().trim().isEmpty()) {
             binding.txtLayoutLname.error = "Excuse me sir, this is a required field"
             binding.txtInputEditLname.requestFocus()
             return false
         } else {
-            binding.txtLayoutFname.isErrorEnabled = false
             binding.txtLayoutLname.isErrorEnabled = false
         }
         return true
@@ -99,10 +119,10 @@ class MainActivity : AppCompatActivity() {
             //check the elements of each fields and apply response.
             when (view.id) {
                 R.id.txtInput_edit_fname -> {
-                    validateName()
+                    validateFirstName()
                 }
                 R.id.txtInput_edit_lname -> {
-                    validateName()
+                    validateLastName()
                 }
                 R.id.txtInput_edit_email -> {
                     validateEmail()
@@ -116,5 +136,33 @@ class MainActivity : AppCompatActivity() {
         override fun afterTextChanged(s: Editable?) {
 
         }
+    }
+}
+
+object ValidateMe {
+
+    val EMAIL_ADDRESS_PATTERN = Pattern.compile("[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+                "\\@" +
+                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+                "(" +
+                "\\." +
+                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+                ")+")
+    val PASSWORD_PATTERN = Pattern.compile("^[a-zA-Z0-9_-]*\$")
+
+    /**
+     * confirm email formatted correctly
+     */
+    fun validEmailFormat(email: String): Boolean {
+        return EMAIL_ADDRESS_PATTERN.matcher(email).matches()
+    }
+
+    fun validPasswordFormat(pwd: String): Boolean {
+        return PASSWORD_PATTERN.matcher(pwd).matches()
+    }
+
+    fun validStringFormat(name: String): Boolean {
+
+        return true
     }
 }
