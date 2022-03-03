a611723faa933b4403ad2b42da66a8a835241f6c
Dauris Little <git@daurislittle.com>
2022-03-03 15:44:55 -0500

    Gestures Control Creation
    Implement Gestures tutorial

---
+++ kotlin/GestureControl/app/src/main/java/com/programmingninja/gesturecontrol/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.programmingninja.gesturecontrol
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
