c235234450c4e3857f793eb5cf34c72acb9f0eff
Dauris Little <git@daurislittle.com>
2022-03-04 11:42:52 -0500

    Update MainActivity.kt
    global lateinit variables and a few variables that are initialized with the value of zero

--- kotlin/GestureControl/app/src/main/java/com/programmingninja/gesturecontrol/MainActivity.kt
+++ kotlin/GestureControl/app/src/main/java/com/programmingninja/gesturecontrol/MainActivity.kt
@@ -2,8 +2,33 @@ package com.programmingninja.gesturecontrol
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.GestureDetector
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.view.GestureDetectorCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
+    /** Global Variables Start **/
+    //gesture
+    lateinit var gdc: GestureDetectorCompat
+
+    ///Textviews
+    lateinit var qv: AppCompatTextView
+    lateinit var sv: AppCompatTextView
+    lateinit var cv: AppCompatTextView
+
+    //Buttons
+    lateinit var ab: FloatingActionButton
+    lateinit var sb: FloatingActionButton
+
+    //integer variables
+    var pts = 0
+    var userAnsw = 0
+    var questionValue1 = 0
+    var questionValue2 = 0
+    /** Global Variables end **/
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
