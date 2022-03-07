d552940d8fb215ea5984d9987dab39cf2e0e4095
Dauris Little <git@daurislittle.com>
2022-03-07 09:11:59 -0500

    Final Touches
    Final touches to make the application accurate

--- kotlin/GestureControl/app/src/main/java/com/programmingninja/gesturecontrol/MainActivity.kt
+++ kotlin/GestureControl/app/src/main/java/com/programmingninja/gesturecontrol/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.programmingninja.gesturecontrol
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.GestureDetector
+import android.view.MotionEvent
+import android.view.VelocityTracker
+import android.view.View
 import android.widget.FrameLayout
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.core.view.GestureDetectorCompat
+import androidx.core.view.MotionEventCompat
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
@@ -24,6 +30,9 @@ class MainActivity : AppCompatActivity() {
     lateinit var ab: FloatingActionButton
     lateinit var sb: FloatingActionButton
 
+    //view
+    lateinit var view:FrameLayout
+
     //integer variables
     var pts = 0
     var userAnsw = 0
@@ -36,7 +45,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //lets do some work
-        var view = findViewById<FrameLayout>(R.id.layout_parent)
+        view = findViewById<FrameLayout>(R.id.layout_parent)
         qv =  findViewById(R.id.tv_question)
         sv = findViewById(R.id.tv_solution)
         cv = findViewById(R.id.tv_points)
@@ -46,16 +55,21 @@ class MainActivity : AppCompatActivity() {
         //assign listeners
         ab.setOnClickListener {
             userAnsw++
-            sv.text = userAnsw.toString()
+            sv.text = "$userAnsw"
         }
 
         sb.setOnClickListener {
             userAnsw--
-            sv.text = userAnsw.toString()
-
+            sv.text = "$userAnsw"
         }
 
         generateQuestion(userAnsw)
+        gdc = GestureDetectorCompat(this, GestureControlListener())
+    }
+
+    override fun onTouchEvent(evt: MotionEvent): Boolean {
+        gdc.onTouchEvent(evt)
+        return true
     }
 
     //generate question function for user
@@ -70,5 +84,42 @@ class MainActivity : AppCompatActivity() {
         val problem = "$questionValue1 + $questionValue2"
         qv.text = problem
     }
-    
+
+    /**
+    *
+    * */
+    inner class GestureControlListener : GestureDetector.SimpleOnGestureListener() {
+
+        override fun onFling(evt1: MotionEvent, evt2: MotionEvent, xVelocity: Float, yVelocity: Float) : Boolean {
+
+            pts -= 75
+            questionValue1 = Random.nextInt(15)
+            questionValue2 = Random.nextInt(25)
+            view.setBackgroundColor(Random.nextInt())
+            qv.text = "$questionValue1 + $questionValue2"
+
+            Snackbar.make(view, "Skipped", Snackbar.LENGTH_SHORT).show()
+            cv.text = "Current Score: $pts"
+            return true
+        }
+
+
+
+        override fun onDoubleTap(evt: MotionEvent?): Boolean {
+
+            if (userAnsw == questionValue1 + questionValue2) {
+                pts = 100*125
+
+                generateQuestion(7)
+            } else {
+                pts -= 125
+                generateQuestion(15)
+                view.setBackgroundColor(Color.RED)
+                Snackbar.make(view, "Incorrect", Snackbar.LENGTH_SHORT).show()
+
+            }
+            cv.text = "Current Score: $pts"
+            return true
+        }
+    }
 }
