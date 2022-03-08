0d2a98975aabef68ffcf87dc3ddacaa45e1a014c
Dauris Little <git@daurislittle.com>
2022-03-08 14:24:09 -0500

    Update SharePrefModule.java
    This is our module class. Basically the module represents our dependencies while component represents a mapping between the module and it’s client/dependent object.

--- java/Dagger2Tutorial/app/src/main/java/com/programmingninja/dagger2tutorial/helper/SharePrefModule.java
+++ java/Dagger2Tutorial/app/src/main/java/com/programmingninja/dagger2tutorial/helper/SharePrefModule.java
@@ -1,5 +1,32 @@
 package com.programmingninja.dagger2tutorial.helper;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
+@Module
 public class SharePrefModule {
+    private Context cx;
+
+    public SharePrefModule(Context context) {
+        this.cx = context;
+    }
+
+    @Singleton
+    @Provides
+    public Context provideContext() {
+        return cx
+    }
+
+    @Singleton
+    @Provides
+    public SharedPreferences provideSharedPreferences(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context)
+    }
 
 }
