ec3b49bcabf3cf15093150e2600cf77464aa6271
Dauris Little <git@daurislittle.com>
2022-03-16 16:27:59 -0400

    NotificationViewer
    ==> frontend created
    + image view added
    + text view added
    + added id to root container
    ==> backend created
    + assigned views
    + display values in view

--- kotlin/AppNotifications/app/src/main/java/com/programmingninja/appnotification/NotificationViewActivity.kt
+++ kotlin/AppNotifications/app/src/main/java/com/programmingninja/appnotification/NotificationViewActivity.kt
@@ -2,10 +2,21 @@ package com.programmingninja.appnotification
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.snackbar.Snackbar
 
 class NotificationViewActivity : AppCompatActivity() {
+    lateinit var parent : ConstraintLayout
+    lateinit var notificationTitle : AppCompatTextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notification_view)
+        parent = findViewById(R.id.rootLayer)
+        notificationTitle = findViewById(R.id.tv_notificationMessage)
+        notificationTitle.text = intent.getStringExtra("title") ?: ""
+
+        Snackbar.make(parent, "Welcome back ", Snackbar.LENGTH_SHORT).show()
     }
 }
