245c593f972a115fe90f0dd176c89958b33525fb
Dauris Little <git@daurislittle.com>
2022-03-16 08:51:52 -0400

    Update Notifier.kt
    To send a notification, we use a notification manager, and provide it with a notification, and an Id. The id should be unique, and could be used to update a notification in future.

--- kotlin/AppNotifications/app/src/main/java/com/programmingninja/appnotification/helper/Notifier.kt
+++ kotlin/AppNotifications/app/src/main/java/com/programmingninja/appnotification/helper/Notifier.kt
@@ -1,11 +1,15 @@
 package com.programmingninja.appnotification.helper
 
+import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
+import android.icu.text.CaseMap
 import android.os.Build
 import androidx.core.app.NotificationChannelCompat
+import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import com.programmingninja.appnotification.R
 
 class Notifier(val context: Context) {
 
@@ -23,8 +27,26 @@ class Notifier(val context: Context) {
             }
              val nManager: NotificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             nManager.createNotificationChannel(setChannel)
+        } else {
+            //
+        }
+    }
 
+    private fun notification(title: String, content: String) : Notification {
 
+        val nBuilder = NotificationCompat.Builder(context, "")
+        nBuilder.setSmallIcon(R.drawable.ic_launcher_background)
+        nBuilder.setContentTitle(title)
+        nBuilder.setContentText(content)
+        nBuilder.priority = NotificationCompat.PRIORITY_DEFAULT
+        return nBuilder.build()
+    }
+
+    fun notificationSend(title: String, content: String) {
+        val notification = notification(title, content)
+        with(NotificationManagerCompat.from(context)) {
+            val nIdentifier = title.hashCode()
+            notify(nIdentifier, notification)
         }
     }
 }
