21371ab1414f8109505a28b4eff389992f533952
Dauris Little <git@daurislittle.com>
2022-03-16 15:22:23 -0400

    Notification Click Action
    Supplying action  to the notification being clicked

---
+++ kotlin/AppNotifications/app/src/main/java/com/programmingninja/appnotification/NotificationViewActivity.kt
@@ -0,0 +1,11 @@
+package com.programmingninja.appnotification
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NotificationViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notification_view)
+    }
+}
