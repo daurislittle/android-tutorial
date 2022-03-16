726db13d9df26a2573ec8bffbeb2cb7db42b728a
Dauris Little <git@daurislittle.com>
2022-03-16 13:38:31 -0400

    Update Notifier.kt
    - removed underscores from variables
    - made global variable for channel id name
    - updated functions name and reference points
    - removed else case

--- kotlin/AppNotifications/app/src/main/java/com/programmingninja/appnotification/helper/Notifier.kt
+++ kotlin/AppNotifications/app/src/main/java/com/programmingninja/appnotification/helper/Notifier.kt
@@ -4,49 +4,51 @@ import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
-import android.icu.text.CaseMap
 import android.os.Build
-import androidx.core.app.NotificationChannelCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.programmingninja.appnotification.R
 
 class Notifier(val context: Context) {
+    val channelIdName = "PokemonReminder"
 
-    private fun createNotification() {
+    init {
+        notificationChannelCreate()
+    }
+    private fun notificationChannelCreate() {
 
         //oreo require channel id to create notification
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val c_Name = ""
-            val c_Description = ""
-            val c_ID = c_Name
-            val c_Importance = NotificationManager.IMPORTANCE_DEFAULT
+            val cName = channelIdName
+            val cDescription = "AppleDes"
+            val cImportance = NotificationManager.IMPORTANCE_DEFAULT
 
-            val setChannel = NotificationChannel(c_ID, c_Name, c_Importance).apply {
-                description = c_Description
+            val setChannel = NotificationChannel(cName, cName, cImportance).apply {
+                description = cDescription
             }
              val nManager: NotificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             nManager.createNotificationChannel(setChannel)
-        } else {
-            //
         }
     }
 
-    private fun notification(title: String, content: String) : Notification {
+    private fun notificationCreate(title: String, content: String) : Notification {
 
-        val nBuilder = NotificationCompat.Builder(context, "")
-        nBuilder.setSmallIcon(R.drawable.ic_launcher_background)
+        val nBuilder = NotificationCompat.Builder(context, channelIdName)
+        nBuilder.setSmallIcon(R.mipmap.ic_launcher_round)
         nBuilder.setContentTitle(title)
         nBuilder.setContentText(content)
         nBuilder.priority = NotificationCompat.PRIORITY_DEFAULT
+
         return nBuilder.build()
     }
 
     fun notificationSend(title: String, content: String) {
-        val notification = notification(title, content)
+        val createNotification = notificationCreate(title, content)
+
         with(NotificationManagerCompat.from(context)) {
             val nIdentifier = title.hashCode()
-            notify(nIdentifier, notification)
+            //notify("", 1800, createNotification)
+            notify(1800, createNotification)
         }
     }
 }
