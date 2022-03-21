dd9e1677fae6c3ff784631744b09991727d669b5
Dauris Little <git@daurislittle.com>
2022-03-21 14:32:45 -0400

    Update ValidateEmail.kt
    class check if input is empty or not then ensure the email address is formatted correctly

--- kotlin/UserInputValidation/app/src/main/java/com/programmingninja/userinputvalidation/helper/ValidateEmail.kt
+++ kotlin/UserInputValidation/app/src/main/java/com/programmingninja/userinputvalidation/helper/ValidateEmail.kt
@@ -1,4 +1,15 @@
 package com.programmingninja.userinputvalidation.helper
 
-class ValidateEmail {
+import android.text.TextUtils
+import androidx.core.text.TextUtilsCompat
+import com.programmingninja.userinputvalidation.helper.validator.BaseValidation
+import com.programmingninja.userinputvalidation.helper.validator.Validate
+
+class ValidateEmail(val emailString: String): BaseValidation() {
+
+    fun validate(): Validate {
+        val isValid = !TextUtils.isEmpty(emailString) && android.util.Patterns.EMAIL_ADDRESS.matcher(emailString).matches()
+
+        return Validate(isValid, if (isValid) "success".hashCode() else "fail".hashCode())
+    }
 }
