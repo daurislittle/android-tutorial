3ddb87f7ac0ad6bc4e2d83e8030f1c5c751c04a8
Dauris Little <git@daurislittle.com>
2022-03-21 14:41:46 -0400

    Update ValidatePassword.kt
    Class checks if empty or not then confirm the password input is complying with expected format

--- kotlin/UserInputValidation/app/src/main/java/com/programmingninja/userinputvalidation/helper/ValidatePassword.kt
+++ kotlin/UserInputValidation/app/src/main/java/com/programmingninja/userinputvalidation/helper/ValidatePassword.kt
@@ -1,4 +1,17 @@
 package com.programmingninja.userinputvalidation.helper
 
-class ValidatePassword {
+import com.programmingninja.userinputvalidation.helper.validator.BaseValidation
+import com.programmingninja.userinputvalidation.helper.validator.Validate
+
+class ValidatePassword(val pass: String) : BaseValidation() {
+
+    val minLength = 6
+    val maxLength = 32
+
+    fun validate(): Validate {
+        if (pass.length < minLength) return Validate(true, "too little".hashCode())
+        if (pass.length > maxLength) return  Validate(true, "too many".hashCode())
+
+        return Validate(true, "success".hashCode())
+    }
 }
