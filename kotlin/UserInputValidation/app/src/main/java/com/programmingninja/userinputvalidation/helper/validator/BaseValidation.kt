44e0effb8945e92f84cd73a4356d6c197942bf72
Dauris Little <git@daurislittle.com>
2022-03-21 11:22:07 -0400

    Create BaseValidation.kt
    validator classes will extend to this class.
    + fun validate(vararg validators: Validator) method to check for multiple formats.

---
+++ kotlin/UserInputValidation/app/src/main/java/com/programmingninja/userinputvalidation/helper/validator/BaseValidation.kt
@@ -0,0 +1,14 @@
+package com.programmingninja.userinputvalidation.helper.validator
+
+class BaseValidation {
+    companion object {
+        fun validate(vararg iValidator: IValidator): Validate {
+            iValidator.forEach {
+                val res = it.validate()
+                if (!res.status) return res
+            }
+
+            return Validate(true, "Successful".hashCode())
+        }
+    }
+}
