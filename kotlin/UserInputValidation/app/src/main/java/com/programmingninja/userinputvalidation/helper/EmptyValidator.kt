535f571a1e988ab88014c8e956b2810a0700b156
Dauris Little <git@daurislittle.com>
2022-03-21 13:45:49 -0400

    Update EmptyValidator.kt
    the method validate() will check input if empty or not then will return Validate value according to the validation result.

--- kotlin/UserInputValidation/app/src/main/java/com/programmingninja/userinputvalidation/helper/EmptyValidator.kt
+++ kotlin/UserInputValidation/app/src/main/java/com/programmingninja/userinputvalidation/helper/EmptyValidator.kt
@@ -1,4 +1,11 @@
 package com.programmingninja.userinputvalidation.helper
 
-class EmptyValidator {
+import com.programmingninja.userinputvalidation.helper.validator.BaseValidation
+import com.programmingninja.userinputvalidation.helper.validator.Validate
+
+class EmptyValidator(val input: String): BaseValidation() {
+    fun validate(): Validate {
+        val isValid = input.isNotEmpty()
+        return Validate(isValid, if (isValid) "success".hashCode() else "right".hashCode())
+    }
 }
