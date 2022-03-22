a3b8ef6ca8ca9476571d82d560caa7794047943a
Dauris Little <git@daurislittle.com>
2022-03-22 11:15:51 -0400

    Update ValidatePassword.kt
    + override function
    -+ replaced with string response
    + check if string values has a special character

--- kotlin/UserInputValidation/app/src/main/java/com/programmingninja/userinputvalidation/helper/ValidatePassword.kt
+++ kotlin/UserInputValidation/app/src/main/java/com/programmingninja/userinputvalidation/helper/ValidatePassword.kt
@@ -1,17 +1,23 @@
 package com.programmingninja.userinputvalidation.helper
 
+import com.programmingninja.userinputvalidation.R
 import com.programmingninja.userinputvalidation.helper.validator.BaseValidation
 import com.programmingninja.userinputvalidation.helper.validator.Validate
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 class ValidatePassword(val pass: String) : BaseValidation() {
 
     val minLength = 6
     val maxLength = 32
+    val specialPattern = Pattern.compile("[^a-zA-Z0-9]")
+    val match: Matcher = specialPattern.matcher(pass)
 
-    fun validate(): Validate {
-        if (pass.length < minLength) return Validate(true, "too little".hashCode())
-        if (pass.length > maxLength) return  Validate(true, "too many".hashCode())
+    override fun validate(): Validate {
+        if (pass.length < minLength) return Validate(false, R.string.txt_validation_pass_error_small)
+        if (pass.length > maxLength) return  Validate(false, R.string.txt_validation_pass_error_long)
+        if (!match.find()) return Validate(false, R.string.txt_validation_pass_error_special)
 
-        return Validate(true, "success".hashCode())
+        return Validate(true, R.string.txt_validation_success)
     }
 }
