a3b380dda5b2f9ad4481a21c533d0732cd32cf46
Dauris Little <git@daurislittle.com>
2022-03-22 11:14:41 -0400

    Update ValidateEmail.kt
    override function and replaced with string reponse

--- kotlin/UserInputValidation/app/src/main/java/com/programmingninja/userinputvalidation/helper/ValidateEmail.kt
+++ kotlin/UserInputValidation/app/src/main/java/com/programmingninja/userinputvalidation/helper/ValidateEmail.kt
@@ -2,14 +2,15 @@ package com.programmingninja.userinputvalidation.helper
 
 import android.text.TextUtils
 import androidx.core.text.TextUtilsCompat
+import com.programmingninja.userinputvalidation.R
 import com.programmingninja.userinputvalidation.helper.validator.BaseValidation
 import com.programmingninja.userinputvalidation.helper.validator.Validate
 
 class ValidateEmail(val emailString: String): BaseValidation() {
 
-    fun validate(): Validate {
+    override fun validate(): Validate {
         val isValid = !TextUtils.isEmpty(emailString) && android.util.Patterns.EMAIL_ADDRESS.matcher(emailString).matches()
 
-        return Validate(isValid, if (isValid) "success".hashCode() else "fail".hashCode())
+        return Validate(isValid, if (isValid) R.string.txt_validation_success else R.string.txt_validation_email_error)
     }
 }
