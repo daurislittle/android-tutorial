5fcd8235068abaa7ff53fccdd64517a2c0c57576
Dauris Little <git@daurislittle.com>
2022-03-22 11:13:43 -0400

    Update EmptyValidator.kt
    override function and inputted string response

--- kotlin/UserInputValidation/app/src/main/java/com/programmingninja/userinputvalidation/helper/EmptyValidator.kt
+++ kotlin/UserInputValidation/app/src/main/java/com/programmingninja/userinputvalidation/helper/EmptyValidator.kt
@@ -1,11 +1,12 @@
 package com.programmingninja.userinputvalidation.helper
 
+import com.programmingninja.userinputvalidation.R
 import com.programmingninja.userinputvalidation.helper.validator.BaseValidation
 import com.programmingninja.userinputvalidation.helper.validator.Validate
 
 class EmptyValidator(val input: String): BaseValidation() {
-    fun validate(): Validate {
+    override fun validate(): Validate {
         val isValid = input.isNotEmpty()
-        return Validate(isValid, if (isValid) "success".hashCode() else "right".hashCode())
+        return Validate(isValid, if (isValid) R.string.txt_validation_success else R.string.txt_validation_error)
     }
 }
