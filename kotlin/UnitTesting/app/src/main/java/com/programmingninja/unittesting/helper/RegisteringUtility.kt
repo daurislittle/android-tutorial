bf04b42ca04a4e1a23f154275ea6ea3e15772c4c
Dauris Little <git@daurislittle.com>
2022-03-24 08:23:04 -0400

    Create RegisteringUtility.kt
    object class created

---
+++ kotlin/UnitTesting/app/src/main/java/com/programmingninja/unittesting/helper/RegisteringUtility.kt
@@ -0,0 +1,22 @@
+package com.programmingninja.unittesting.helper
+
+object RegisteringUtility {
+
+        private val UserExist = listOf("James","Randall","Bruce", "Ryan")
+
+        /**
+         *
+         * inputs will test the following
+         * should not be empty
+         * username is taken
+         * passwords do not match
+         * password does not meet criteria
+         * agrees to terms
+         * */
+
+        fun inputValidation(fName: String, lName: String, uName: String, pass: String, pass2: String, terms: Boolean): Boolean {
+
+            return false
+        }
+
+}
